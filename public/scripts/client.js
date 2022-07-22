77ea161154cdd9e6d07452cc86b1e5693a75f5ef
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-07-22 01:05:30 +0000

    add comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,11 +1,8 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(document).ready(function() {
 
+  // creat format new tweet with response tweet data
   const createTweetElement = function(tweetDataObj) {
+
     const markup = `<article class="show-tweet">
     <h5>
       <div> <img class="smallAvatar" src="${tweetDataObj.user.avatars}"> <p class="username">${tweetDataObj.user.name}</p> </div>
@@ -19,11 +16,16 @@ $(document).ready(function() {
       <p> <i class="fa-solid fa-flag iconHover"></i> <i class='fas fa-retweet iconHover'></i> <i class="fa-solid fa-heart iconHover"></i> </p>
     </footer>
   </article>`;
+
     return markup;
   };
 
   const creatAlertMessage = function(message) {
+
+    //clear the message before append
     $("#alertMessage").empty();
+
+    //if user compose empty tweet
     if (message === "emptyAlert") {
       const alert = `<i class="fa-solid fa-triangle-exclamation"></i>
       Tweet contents should not be empty.
@@ -31,6 +33,7 @@ $(document).ready(function() {
       $("#alertMessage").append(alert);
     }
 
+    //if the tweet content exceed 140
     if (message === "lengthAlert") {
       const alert = `<i class="fa-solid fa-triangle-exclamation"></i>
       Too long. Tweet contents should not exceed 140 letters.
@@ -38,28 +41,30 @@ $(document).ready(function() {
       $("#alertMessage").append(alert);
     }
 
-
   };
 
+  //get styled tweet and append each exist tweet from database
   const renderTweets = function(tweetDataArr) {
+    
+    //clear container before append
     $('#tweets-container').empty();
+
     for (const tweetDataObj of tweetDataArr) {
       const $tweet = createTweetElement(tweetDataObj);
       $('#tweets-container').append($tweet);
     }
   };
 
-  //get response when submit
+  //get response
   const loadTweets = function() {
-    console.log("success call loadtweets");
+
+    // hide compose tweet box
     $("#new-tweet").slideUp("fast");
 
     $.ajax({
-      method: "GET",
       url: "/tweets/",
       success: (response) => {
-        console.log(response);
-        renderTweets(response.reverse());
+        renderTweets(response.reverse()); //show new tweet on the top
       },
       error: (error) => {
         console.log(error);
@@ -68,40 +73,43 @@ $(document).ready(function() {
 
   };
 
+  //make sure page show all tweets
   loadTweets();
 
-  // for hide alert box
+  // when text area is re-clicked, hide alert box
   $('#tweet-text').click(() => {
     $("#alertMessage").empty();
     $("#alertMessage").slideUp("fast");
   });
 
-  //post when submit
   $('#submitTweet').submit(function(event) {
 
+    //prevent the origin behaviour of submit event
     event.preventDefault();
 
-    const textData = $(this).serialize();
+    const textData = $(this).serialize(); // change input text format
     const tweetLength = $('#tweet-text').val().length;
 
+    //if empty tweet content, show alert
     if (tweetLength === 0) {
       $("#alertMessage").slideDown("slow", creatAlertMessage("emptyAlert"));
     } else {
+
+      //if exceed max length, show alert
       if (tweetLength > 140) {
         $("#alertMessage").slideDown("slow", creatAlertMessage("lengthAlert"));
       } else {
 
+        
+        $('#tweet-text').val("");//after submition, empty text area
+        $('#char-counter').text("140");//after submition, set the counter back to maximum
 
-        //empty text area
-        $('#tweet-text').val("");
-        $('#char-counter').text("140");
-        //post
         $.ajax({
           method: "POST",
           url: "/tweets/",
           data: textData,
           success: () => {
-            loadTweets();
+            loadTweets();// show new tweet immidiately
           },
           error: (error) => {
             console.log(error);
@@ -112,22 +120,29 @@ $(document).ready(function() {
 
   });
 
-  //arrow effect 
+  //if the expending icon on nav bar is clicked, slide new tweet box up or down 
   $("#navAngleIcon").click(function() {
+
     $("#new-tweet").slideToggle("slow");
-    $("#tweet-text").focus();
+    $("#tweet-text").focus();//set the cursor immidiately
+    
   });
 
   //scroll up button
   $(window).scroll(function() {
-    console.log($(document).scrollTop())
+
+    //if current position hight higher than 300px, display the button
     if ($(document).scrollTop() > 300) {
       $("#scrollUpButton").css("display", "block");
     } else {
+
+      //otherwise hide
       $("#scrollUpButton").css("display", "none");
     }
+
   });
 
+  //if the scrollup button is clicked, scroll to the top
   $("#scrollUpButton").on("click", function() {
     $(document).scrollTop(0);
   });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,16 @@
+//count composed text char
+//max char is 140
 $(document).ready(function() {
+
   $('#tweet-text').on("input", () => {
+
+    //get input char
     const len = $('#tweet-text').val().length;
+    
+    //reduce counter while user typing
     $('#char-counter').text(140 - len);
+
+    //change counter color when exceed maximum
     if (len > 140) {
       $('#char-counter').addClass("counter-red");
     } else {
@@ -9,7 +18,9 @@ $(document).ready(function() {
         $('#char-counter').removeClass("counter-red");
       }
     }
+
   });
+
 });
 
 
